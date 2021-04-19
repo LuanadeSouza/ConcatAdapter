7eda207ef8ce7eab6acf690ce6fdd9f7fac43b84
Luana Souza <luana.souza@ifood.com.br>
2021-04-19 12:50:53 -0300

    add     mavenCentral()

--- app/src/main/java/br/com/luanadev/concatadapter/MainActivity.kt
+++ app/src/main/java/br/com/luanadev/concatadapter/MainActivity.kt
@@ -4,19 +4,19 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.ConcatAdapter
 import br.com.luanadev.concatadapter.databinding.ActivityMainBinding
+import by.kirich1409.viewbindingdelegate.viewBinding
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivityMainBinding
-
+    private val binding by viewBinding {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
     private lateinit var programmingLanguagesAdapter: ProgrammingLanguagesAdapter
     private lateinit var tipsAdapter: TipsAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
         initRecyclerView()
     }
 
